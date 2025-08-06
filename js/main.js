b08a4d877ffec44dcbca2454f4b8c86814fd8e10
Delano <del.191006@gmail.com>
2025-08-06 08:41:55 +0700

    Navigation Button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 "use strict";
+
+//Handling Open and Close features
+const featuresBtn = document.querySelectorAll("button");
+featuresBtn.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const pages = btn.dataset.page;
+    const page = document.getElementById(`page-${pages}`);
+    page.classList.toggle("hidden");
+  });
+});
